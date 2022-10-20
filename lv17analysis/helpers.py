6770e96f1e33432b8f996b8059b2c763af336d68
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2022-10-20 04:48:05 -0700

    Add functions for clearing output. Add experimental plotting class 'Tfig' Add 'nargout' function for optional number of output arguments Add functions for creating ring (a) data and (b) plotting it into existing image. Add function for getting a sorted list of h5 filenames from folder.

--- lv17analysis/helpers.py
+++ lv17analysis/helpers.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pickle as pkl
-from IPython.display import display, clear_output
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from IPython.display import display
+from IPython.display import clear_output as clear_out
 import datetime
+import traceback
+from glob import glob
+import os.path
 
 
 def fftshift(sig):
@@ -83,13 +90,23 @@ def bindata(data, bins=50):
     return ret
 
 
-def movmean(dlist, k=5):
+def movmean(y, k=5):
     kk = np.arange(k)
-    return np.convolve(dlist, np.ones_like(kk), 'valid') / k
+    return np.convolve(y, np.ones_like(kk), 'valid') / k
 
 
 def clear():
-    clear_output(wait=True)
+    clear_out(wait=True)
+    return
+
+
+def clear_output(wait=True):
+    clear_out(wait=wait)
+    return
+
+
+def clc(wait=False):
+    clear_output(wait=wait)
     return
 
 
@@ -100,3 +117,91 @@ def today():
 def now():
     return str(datetime.datetime.now())
 
+
+class Tfig:
+
+    curr_ax = np.asarray(0, dtype=int)
+    curr_ax_pos = np.asarray((0,0), dtype=int)
+    cmap = 'magma'
+
+
+    def __init__(self, rows=1, cols=1, *args, **kwargs):
+        self.rows = rows
+        self.cols = cols
+        self.shape = (rows, cols)
+        self.fig, self.ax = plt.subplots(rows, cols, squeeze=False, *args, **kwargs)
+        # self.ax_shape = np.shape()
+        self._args = args
+        self._kwargs = kwargs
+
+
+
+    def imshow(self, img, title=None, *args, **kwargs):
+
+        ax = self.gca()
+        im = ax.imshow(img, cmap=self.cmap)
+        ax.axis('image')
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes('right', size='5%', pad=0.025)
+        self.fig.colorbar(im, cax=cax, orientation = 'vertical')
+
+        if title is not None:
+            ax.title(title)
+
+        self.curr_ax += 1
+
+        return im
+
+
+    def gca(self, pos=None):
+        if pos is not None:
+            if len(np.shape((pos))) == 0:
+                self.curr_ax = pos
+            else:
+                self.curr_ax = pos[0]*self.ax.shape[1] + pos[1]
+
+        self.curr_ax_pos = divmod(self.curr_ax, self.ax.shape[1])
+        return self.ax[self.curr_ax_pos]
+
+
+def nargout(*args):
+    callInfo = traceback.extract_stack()
+    callLine = str(callInfo[-3].line)
+    split_equal = callLine.split('=')
+    split_comma = split_equal[0].split(',')
+    num = len(split_comma)
+    return args[0:num] if num > 1 else args[0]
+
+
+def ring_data(radii=[75, 150], center=[403, 677]):
+    th = np.append(np.linspace(0,2*np.pi,100), np.nan)
+    th = np.expand_dims(th, -1)
+    th = np.repeat(th, len(radii), axis=1)
+    x = np.cos(th) * np.asarray(radii) + center[1]
+    y = np.sin(th) * np.asarray(radii) + center[0]
+    return x, y
+
+
+def plot_rings(radii=[75, 150], center=[403, 677], ax=None, ls='--', lw=1, c='tab:red',
+               alpha=.75, label=None, keep_lims=False):
+    x,y = ring_data(radii=radii, center=center)
+    if ax is None:
+        fig, ax = plt.subplots(1,1)
+    if keep_lims:
+        lims = ax.get_xlim(), ax.get_ylim()
+    pt = ax.plot(x, y, c=c, ls=ls, lw=lw, label=label, alpha=alpha)
+    if keep_lims:
+        ax.set_xlim(lims[0])
+        ax.set_ylim(lims[1])
+    if label is not None:
+        ax.legend()
+    return nargout(pt)
+
+
+def get_filenames(fp):
+    return np.sort(glob(os.path.join(fp, 'r*.h5')))
+
+
+def fn2run(fn):
+    return [int(f.split('/r')[-1].split('_')[0]) for f in fn]
